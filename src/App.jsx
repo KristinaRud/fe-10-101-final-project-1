b2801071c1926a2d5cb1fc4e340f9978f48fc9c0
@kristina.rud5 <kristina.rud5@gmail.com>
2023-05-23 15:57:39 +0300

    added prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import AppRoute from './route/app.route';
-import store from './store';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import AppRoute from "./route/app.route";
+import store from "./store";
 
 const App = () => (
   <Provider store={store}>
