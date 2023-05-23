07a27f6c4d406a46f7132f786a14685000a45e39
KristinaRud <kristina.rud5@gmail.com>
2023-05-24 01:07:08 +0300

    create footer branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import AppRoute from "./route/app.route";
 import store from "./store";
+import Footer from "./components/Footer/Footer";
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
       <AppRoute />
+      <Footer />
     </BrowserRouter>
   </Provider>
 );
