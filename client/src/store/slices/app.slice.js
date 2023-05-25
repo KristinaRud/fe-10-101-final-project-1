977887f9be9f9ff8646d276eaa0a313fb54f2bb7
Volodymyr Mazepa <mazepav88@gmail.com>
2023-05-25 20:50:05 +0300

    Add: BackEnd with MongoDB Credentials

---
+++ client/src/store/slices/app.slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const appSlice = createSlice({
+  name: "app",
+  initialState: {},
+  reducers: {
+    actionFirst: () => {},
+  },
+});
+
+export const { actionFirst } = appSlice.actions;
+
+export default appSlice.reducer;
