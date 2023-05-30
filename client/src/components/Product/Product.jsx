b8ea906fbaef6f0fd0dbad376175ae5169098481
Anna Simaniuk <Anna.shchavinska@gmail.com>
2023-05-30 22:36:33 +0300

    add product

---
+++ client/src/components/Product/Product.jsx
@@ -0,0 +1,28 @@
+import { Typography } from "@mui/material";
+
+const Product = () => {
+  return (
+    <Card>
+      <Typography />
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default Product;
--- client/src/route/app.route.js
+++ client/src/route/app.route.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import NotPage from "../pages/NotPage/NotPage";
-import HomePage from "../pages/Home/Home";
 import LoginPage from "../pages/Login/Login";
 import AboutUsPage from "../pages/AboutUs/AboutUsPage";
+import Product from "../components/Product/Product";
 
 const AppRoute = () => (
   <Routes>
-    <Route path="/" element={<HomePage />} />
+    <Route path="/" element={<Product />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/about" element={<AboutUsPage />} />
     <Route path="*" element={<NotPage />} />
