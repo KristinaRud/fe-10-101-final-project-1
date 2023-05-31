4ebe3412959022b7a5cf7a5f12d6ded6f6c01a65
Anna Simaniuk <Anna.shchavinska@gmail.com>
2023-05-31 23:28:49 +0300

    fixed bugs

--- client/src/route/app.route.js
+++ client/src/route/app.route.js
@@ -2,11 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import NotPage from "../pages/NotPage/NotPage";
 import LoginPage from "../pages/Login/Login";
 import AboutUsPage from "../pages/AboutUs/AboutUsPage";
-import CategoriesList from "../components/CategoriesProducts/CategoriesList/CategoriesList";
+import HomePage from "../pages/Home/Home";
 
 const AppRoute = () => (
   <Routes>
-    <Route path="/" element={<CategoriesList />} />
+    <Route path="/" element={<HomePage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/about" element={<AboutUsPage />} />
     <Route path="*" element={<NotPage />} />
