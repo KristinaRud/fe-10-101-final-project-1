3df438e132722561baa5604f30a1a8c97fd2d2cc
Anna Simaniuk <Anna.shchavinska@gmail.com>
2023-06-02 21:21:25 +0300

    add breadcrumbs, category title

--- client/src/components/CategoriesProducts/CategoriesList/CategoryItem/CategoryItem.jsx
+++ client/src/components/CategoriesProducts/CategoriesList/CategoryItem/CategoryItem.jsx
@@ -3,13 +3,16 @@ import { ListItem, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import s from "./CategoryItem.module.scss";
 
-const CategoryItem = ({ imgUrl, name }) => {
+const CategoryItem = ({ imgUrl, name, id }) => {
   return (
     <ListItem className={s.wrapper} sx={{ backgroundImage: `url(${imgUrl})` }}>
       <Typography variant="h6" className={s.name}>
         {name}
       </Typography>
-      <Link to={`${name}?categories=${name}`} className={s.link}>
+      <Link
+        to={`${id}?categories=${id}&perPage=8&startPage=1&sort=-rating`}
+        className={s.link}
+      >
         See All Products
       </Link>
     </ListItem>
@@ -19,6 +22,7 @@ const CategoryItem = ({ imgUrl, name }) => {
 CategoryItem.propTypes = {
   imgUrl: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default CategoryItem;
