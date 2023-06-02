f85e67ba6e1c618c4a429b0d100e7d7f10b1fccd
Maryna Spyrydonova <m.spyrydonova@gmail.com>
2023-06-03 00:51:55 +0300

    added contactUs page

--- client/src/route/app.route.js
+++ client/src/route/app.route.js
@@ -3,12 +3,14 @@ import NotPage from "../pages/NotPage/NotPage";
 import HomePage from "../pages/Home/Home";
 import LoginPage from "../pages/Login/Login";
 import AboutUsPage from "../pages/AboutUs/AboutUsPage";
+import ContactUs from "../pages/ContactUs/ContactUs";
 
 const AppRoute = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/about" element={<AboutUsPage />} />
+    <Route path="/contact" element={<ContactUs />} />
     <Route path="*" element={<NotPage />} />
   </Routes>
 );
