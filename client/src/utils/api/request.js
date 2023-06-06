edbf941d7ec4900429da2e1ad1d3a44bddca3f89
Anna Simaniuk <Anna.shchavinska@gmail.com>
2023-06-06 09:38:04 +0300

    add axios, change utils, change store, add comparison products in backend, add checkout route

--- client/src/utils/api/request.js
+++ client/src/utils/api/request.js
@@ -1,8 +1,18 @@
 import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { setAuthToken } from "./setAuthToken";
 
 const request = async ({ url, method = "GET", body, headers } = {}) => {
+  const token = window.localStorage.getItem("token");
+  const decoded = token ? jwtDecode(token) : null;
+
   axios.defaults.baseURL = "http://localhost:4000/api";
   axios.defaults.headers.post["Content-Type"] = "application/json";
+
+  if (token) {
+    setAuthToken(token);
+    console.log(decoded);
+  }
 
   const fetchData = () => {
     if (method === "GET") return axios.get(url, { params: body });
