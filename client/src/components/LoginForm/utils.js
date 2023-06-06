3289c6d4d4f5225992c516c208b62fd8a8365540
Anna Simaniuk <Anna.shchavinska@gmail.com>
2023-06-06 17:10:49 +0300

    add login logic, token fn

--- client/src/components/LoginForm/utils.js
+++ client/src/components/LoginForm/utils.js
@@ -5,11 +5,11 @@ export const validationSchema = Yup.object({
     .email("Invalid email address")
     .required("Required"),
   password: Yup.string()
-    // .min(8, "Password must be at least 8 characters")
-    // .max(20, "Password must not exceed 20 characters")
-    // .matches(
-    //   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/,
-    //   "Password must contain at least one uppercase letter, one lowercase letter, and one digit",
-    // )
+    .min(8, "Password must be at least 8 characters")
+    .max(20, "Password must not exceed 20 characters")
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, and one digit",
+    )
     .required("Password is required"),
 });
