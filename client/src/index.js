cfc63164ffed61dea59cce5ad1fc83109fdec967
MaverickUKR <mazepav88@gmail.com>
2023-06-21 14:07:57 +0300

    Small bugs fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/styles/reset.css";
 
-const root = document.getElementById("root");
-render(<App />, root);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
