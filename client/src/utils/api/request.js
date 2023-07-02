19b402add0a561adfa02eeecc6ea2ac31f6d8f62
suheckiy_m <suheckiymax@gmail.com>
2023-07-02 22:02:31 +0300

    update baseUrl

--- client/src/utils/api/request.js
+++ client/src/utils/api/request.js
@@ -4,7 +4,7 @@ import { checkToken } from "./checkToken";
 const request = async ({ url, method = "GET", body, headers } = {}) => {
   const token = window.localStorage.getItem("token");
 
-  axios.defaults.baseURL = "https://technokit-store.onrender.com/";
+  axios.defaults.baseURL = `${window.location.href}api`;
   axios.defaults.headers.post["Content-Type"] = "application/json";
 
   if (token) {
